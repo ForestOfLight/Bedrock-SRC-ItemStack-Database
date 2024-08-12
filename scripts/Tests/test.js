0d409e74be48aa47c3563aba7f5aa24b156a57a9
gameza_src <120748737+gamezaSRC@users.noreply.github.com>
2024-08-12 17:36:41 -0600

    Update test.js

--- scripts/Tests/test.js
+++ scripts/Tests/test.js
@@ -1,15 +1,28 @@
 import { Player, world, ItemStack } from "@minecraft/server";
 import ItemManager from "../main";
 
-const iManager = new ItemManager('myTable'), start = Date.now();
-world.afterEvents.worldInitialize.subscribe(() => {
-    console.warn('Time to fully load: ' + (Date.now() - start) + 'ms');
-    iManager.set('XD', new ItemStack('minecraft:stone', 64));
+/**
+ * 
+ * First we need to wait for the world to initialize, then we can create a new instance of the ItemManager class.
+ * This is because the Database needs to load all the saved structures in order to get the items.
+ * @type {ItemManager}
+ */
+let IManager;
+world.afterEvents.worldInitialize.subscribe(() => system.runTimeout(() => IManager = new ItemManager('myTable'), 200));
+
+world.afterEvents.itemUse.subscribe(({ source: player, itemStack: item }) => {
+    if (!(player instanceof Player)) return;
+    if (!item || item.type !== 'minecraft:apple') return;
+    const start = Date.now();
+    IManager.save('myItem', new ItemStack('minecraft:apple', 64));
     console.warn('Time to fully save an Item: ' + (Date.now() - start) + 'ms');
-});
+})
 
 world.afterEvents.itemUse.subscribe(({ source: player }) => {
     if (!(player instanceof Player)) return;
-    const item = iManager.get('XD'), inv = player.getComponent('minecraft:inventory').container;
+    if (!item || item.type !== 'minecraft:stick') return;
+    const item = IManager.get('myItem'), inv = player.getComponent('minecraft:inventory').container;
     inv.addItem(item);
+    IManager.delete('myItem');
+    console.log('Item added to inventory');
 })
